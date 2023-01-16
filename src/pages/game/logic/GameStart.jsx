2dd26a347d8bc847ef7bb400caf7599146a05472
hackelmo <tokkito88@gmail.com>
2023-01-17 06:58:18 +0900

    [FEAT] 게임화면 레이아웃 구현완료

--- src/pages/game/logic/GameStart.jsx
+++ src/pages/game/logic/GameStart.jsx
@@ -1,52 +1,44 @@
-import { useQuery } from '@tanstack/react-query';
-import React, { useEffect, useRef, useState } from 'react'
-import io from 'socket.io-client'
-import styled from 'styled-components';
+// import React, { useEffect, useRef, useState } from "react";
+// import io from "socket.io-client";
 
-const GameStart = () => { // const {room} = useParams()
-  const [black, setBlack] = useState('')
-  const [peers, setPeers] = useState([]) // peers =[1,2,3,4]
-  const [gameChat, setGameChat] = useState([])
-  const room =4
-  const userID = 3
-  const socket = useRef();
-  const rooms = useQuery(['roomInfo'],()=>{return console.log('hi')})
+// const socket = io.connect(process.env.REACT_APP_SERVER);
 
-  const selectFirstCarddHandler = (e)=>{
-    socket.current.emit('selectFirstCard', userID, black)
-    setBlack('')
-  }
-  const setPeersHandler= (roomUser, gameChat)=>{
-    setPeers(prev=>[...prev,roomUser])
-    setGameChat(prev=>[...prev,gameChat])
-  }
+// const GameStart = () => {
+//   // const {room} = useParams()
+//   const [black, setBlack] = useState(0);
+//   const [selected, setSelected] = useState(false);
+//   const { userId, roomId } = useSelector((state) => state.gameSlice);
 
-   useEffect(()=>{
-    socket.current = io.connect(process.env.REACT_APP_SERVER);
-    socket.current.emit('gameStart', room, userID)
-    socket.current.on('joinedGame', setPeersHandler)
-    return () => {
-      socket.current.disconnect();
-    };
-  }, [])
+//   console.log(userId, roomId);
 
-  useEffect(()=>{
-  },[socket])
-  
-  if(peers.length!==rooms.number) return <div>...접속중</div> // 리액트 조기반환
+//   const selectFirstCarddHandler = (e) => {
+//     socket.emit("selectFirstCard", roomId, userId, black);
+//     setSelected(true);
+//   };
+//   useEffect(() => {
+//     // socket.emit("gameStart", roomId, userId);
+//     return () => {
+//       socket.disconnect();
+//     };
+//   }, []);
 
-  return (
-    <div>
-      {peers.map((video,i)=>{
-        return (<div key={`peers${i}`}>
-          <video>{video}</video>
-          <div>{gameChat[i]}</div>
-        </div>)
-      })}
-      <input type='text' value={black} onChange={(e)=>setBlack(e.target.value)}/>
-      <button onClick={selectFirstCarddHandler}>패 결정</button>
-    </div>
-  )
-}
+//   useEffect(() => {}, [socket]);
+//   if (selected)
+//     return (
+//       <>
+//         <div>다른플레이어를 기다려주세요</div>
+//       </>
+//     );
+//   return (
+//     <div>
+//       <input
+//         type="number"
+//         value={black}
+//         onChange={(e) => setBlack(e.target.value)}
+//       />
+//       <button onClick={selectFirstCarddHandler}>확인</button>
+//     </div>
+//   );
+// };
 
-export default GameStart
+// export default GameStart;
