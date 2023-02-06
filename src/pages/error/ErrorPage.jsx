cc66845637c5d05690cf047035190756fbf12d2c
hackelmo <tokkito88@gmail.com>
2023-02-06 23:25:45 +0900

    [FIX] 엔딩모달 UI 변경

--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import { IMG } from '../../helpers/image';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { IMG } from "../../helpers/image";
+import { StButton } from "../Button";
 
 const ErrorPage = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <StWrapper>
-      <StError src={IMG.gameError} alt="에러발생"/>
-      <StBtn color="#ffdf24" onClick={()=>navigate('/')}>돌아가기</StBtn>
+      <StError src={IMG.gameError} alt="에러발생" />
+      <StButton variant="primary" size="md" onClick={() => navigate("/")}>
+        돌아가기
+      </StButton>
     </StWrapper>
-  )
-}
+  );
+};
 
-export default ErrorPage
-
+export default ErrorPage;
 
 const StWrapper = styled.div`
   display: flex;
@@ -27,20 +29,4 @@ const StWrapper = styled.div`
 const StError = styled.img`
   width: 570px;
   height: 436px;
-`
-const StBtn = styled.button`
-  width: 100px;
-  height: 32px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: solid 1px #000;
-  background-color: ${(props) => props.color};
-  border-radius: 6px;
-  font-weight: 700;
-  font-size: 18px;
-  line-height: 100%;
-  text-align: center;
-  color: #000;
-  box-shadow: 0 3px 0 0 #000;
 `;
--- src/pages/game/ele/EndingModal.jsx
+++ src/pages/game/ele/EndingModal.jsx
@@ -3,10 +3,14 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import useSound from "use-sound";
-import Modal from "../../../components/form/modal/Modal";
+
+import Moddal from "../../../components/form/modal/Moddal";
 import { ICON } from "../../../helpers/Icons";
 import { Sounds } from "../../../helpers/sounds";
+import { BootStrap } from "../../BootStrap";
+
 const EndingModal = ({ ending, endingHandler }) => {
+  const { StWrapper } = BootStrap;
   const [play] = useSound(Sounds.Test);
   useEffect(() => {
     play();
@@ -14,105 +18,182 @@ const EndingModal = ({ ending, endingHandler }) => {
   const { endingInfo } = useSelector((state) => state.gameSlice);
 
   return (
-    <Modal
+    <Moddal
       width="628px"
       height="438px"
-      modal={ending.toString()}
+      modal={ending}
       closeModal={endingHandler}
     >
-      <StGameEndingWrapper>
-        <StRankContainer>
-          <StGameOverImg src={ICON.RankGameOver} alt="게임오버"></StGameOverImg>
-          <StUserContainer>
-            <StRankBox one={true}>
-              <img src={ICON.RankOne} alt="1등" />
-              <StNick>{endingInfo[0]?.userName}</StNick>
-              <StAdd>+30</StAdd>
-              <StScore>{endingInfo[0]?.score}</StScore>
+      <StWrapper padding="30px" gap="26px">
+        <Stimg src={ICON.RankGameOver} alt="게임오버" />
+        <StUserContainer>
+          <StRankBox one={true}>
+            <StDescBox>
+              <Stimg width="19px" height="20px" src={ICON.RankOne} alt="1등" />
+              <div>{endingInfo[0]?.userName}</div>
+            </StDescBox>
+            <StDescBox>
+              <div>{endingInfo[0]?.score}</div>
+              <StScoreUp>
+                {endingInfo[3] ? "+100" : endingInfo[2] ? "+70" : "+30"}
+              </StScoreUp>
+            </StDescBox>
+          </StRankBox>
+          {endingInfo[1] ? (
+            <StRankBox>
+              <StDescBox>
+                <Stimg
+                  width="19px"
+                  height="20px"
+                  src={ICON.RankSecond}
+                  alt="2등"
+                />
+                <div>{endingInfo[1]?.userName}</div>
+              </StDescBox>
+              <StDescBox>
+                <div>{endingInfo[1]?.score}</div>
+                <StScoreUp color={endingInfo[3] ? "#00831d" : "#aaa"}>
+                  {endingInfo[3] ? "+50" : endingInfo[2] ? "+40" : "-10"}
+                </StScoreUp>
+              </StDescBox>
             </StRankBox>
-            {endingInfo[1] ? (
-              <StRankBox>
-                <img src={ICON.RankSecond} alt="2등" />
-                <StNick>{endingInfo[1]?.userName}</StNick>
-                <StAdd>+10</StAdd>
-                <StScore>{endingInfo[1]?.score}</StScore>
-              </StRankBox>
-            ) : (
-              <StRankNullBox />
-            )}
-            {endingInfo[2] ? (
-              <StRankBox>
-                <img src={ICON.RankThird} alt="3등" />
-                <StNick>{endingInfo[2].userName}</StNick>
-                <StAdd>+0</StAdd>
-                <StScore>{endingInfo[2].score}</StScore>
-              </StRankBox>
-            ) : (
-              <StRankNullBox />
-            )}
-            {endingInfo[3] ? (
-              <StRankBox>
-                <img src={ICON.RankForth} alt="4등" />
-                <StNick>{endingInfo[3].userName}</StNick>
-                <StAdd>+0</StAdd>
-                <StScore>{endingInfo[3].score}</StScore>
-              </StRankBox>
-            ) : (
-              <StRankNullBox />
-            )}
-          </StUserContainer>
-          <button onClick={endingHandler}>게임 로비로 돌아가기</button>
-        </StRankContainer>
-      </StGameEndingWrapper>
-    </Modal>
+          ) : (
+            <StRankNullBox></StRankNullBox>
+          )}
+          {endingInfo[2] ? (
+            <StRankBox>
+              <StDescBox>
+                <Stimg
+                  width="19px"
+                  height="20px"
+                  src={ICON.RankThird}
+                  alt="3등"
+                />
+                <div>{endingInfo[2]?.userName}</div>
+              </StDescBox>
+              <StDescBox>
+                <div>{endingInfo[2]?.score}</div>
+                <StScoreUp color={endingInfo[3] ? "#00831d" : "#aaa"}>
+                  {endingInfo[3] ? "+30" : "-20"}
+                </StScoreUp>
+              </StDescBox>
+            </StRankBox>
+          ) : (
+            <StRankNullBox></StRankNullBox>
+          )}
+          {endingInfo[3] ? (
+            <StRankBox>
+              <StDescBox>
+                <Stimg
+                  width="19px"
+                  height="20px"
+                  src={ICON.RankForth}
+                  alt="4등"
+                />
+                <div>{endingInfo[3]?.userName}</div>
+              </StDescBox>
+              <StDescBox>
+                <div>{endingInfo[3]?.score}</div>
+                <StScoreUp color="#aaa">-30</StScoreUp>
+              </StDescBox>
+            </StRankBox>
+          ) : (
+            <StRankNullBox></StRankNullBox>
+          )}
+        </StUserContainer>
+        <StBtn onClick={endingHandler}>확인</StBtn>
+      </StWrapper>
+    </Moddal>
   );
 };
 
 export default EndingModal;
 
-const StGameEndingWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  padding: 30px;
-  width: 628px;
-  height: 438px;
+const Stimg = styled.img`
+  width: ${({ width }) => width || "220px"};
+  height: ${({ height }) => height || "102px"};
+  object-fit: contain;
 `;
-const StRankContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
-`;
+
 const StUserContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 6px;
-  margin: 20px 0;
 `;
 const StRankBox = styled.div`
+  font-size: 14px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1;
+  letter-spacing: normal;
+  text-align: left;
+  color: #111;
+
   display: flex;
   justify-content: space-between;
+  align-items: center;
   width: 468px;
   height: 40px;
   border-radius: 4px;
+  padding: 10px 20px;
+
   border: ${({ one }) => {
-    return one ? "1px solid black" : "1px solid #ddd";
+    return one ? "1px solid #000" : "1px solid #ddd";
   }};
   background-color: ${({ one }) => {
     return one ? "#ffdf24" : "#f1f1f1";
   }};
 `;
+const StDescBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+`;
+
+const StScoreUp = styled.div`
+  width: 42px;
+  height: 20px;
+  font-size: 12px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1;
+  letter-spacing: normal;
+  text-align: left;
+  color: #000;
+  border-radius: 999px;
+  background-color: ${({ color }) => color || "#ff601c"};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StBtn = styled.button`
+  margin-top: 10px;
+  width: 150px;
+  height: 32px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: solid 1px #000;
+  background-color: #009320;
+  border-radius: 6px;
+  box-shadow: 0 3px 0 0 #000;
+
+  font-size: 14px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1;
+  letter-spacing: normal;
+  text-align: center;
+  color: #000;
+`;
+
 const StRankNullBox = styled.div`
   width: 468px;
   height: 40px;
   border: none;
   background-color: none;
 `;
-const StGameOverImg = styled.img`
-  width: 220px;
-  height: 102px;
-  object-fit: contain;
-`;
-const StNick = styled.span``;
-const StAdd = styled.span``;
-const StScore = styled.span``;
